2f7b0eddb201ffe897b5f315411a2a0fd8e5a0ea
SelfJosh <99226546+SelfJosh@users.noreply.github.com>
2022-03-23 01:07:07 -0600

    Changed startup page and home video

--- JS/page1.js
+++ JS/page1.js
@@ -1,5 +1,4 @@
-document.getElementById("click").disabled = true;
-setTimeout(function(){document.getElementById("click").disabled = false}, 6100);
+
 document.getElementById("click").onclick = function () {
     location.href = "./Home.html";
 };
